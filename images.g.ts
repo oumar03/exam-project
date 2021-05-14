8bd15b3df359ca9710773784c72548815450ee98
oumar03 <82471862+oumar03@users.noreply.github.com>
2021-05-14 15:45:50 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,123 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "aqK#_c!C`lZ)wqC?`[Ba":
+            case "Football Player":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . 2 2 2 2 2 e . . . 
+. . . . . . 2 2 2 2 d 2 2 e . . 
+. . . . . e 2 2 2 2 2 2 2 e . . 
+. . . . . e 2 2 2 2 2 2 2 e . . 
+. . . . . e 2 2 2 2 2 e f f c c 
+. . . . . e e 2 2 e f f f f b c 
+. . . e e e f e 2 2 b f f f d c 
+. . e e 2 2 d f e 2 1 1 1 1 b c 
+. . e e 2 2 d f e e e c c c . . 
+. . b 1 1 d e 2 2 e e c . . . . 
+. . . f f f d d 2 2 f d d . . . 
+. . . . f f d d e e f d d . . . 
+. . . . . f f f f f . . . . . . 
+. . . . e e e f f . . . . . . . 
+. . . . e e e e f f . . . . . . 
+`;
+            case "30+htez2q1!hfQIZG^zT":
+            case "goal post":return img`
+.............cc.
+............cbbc
+............cbbc
+...........bdcc.
+...........bdbb.
+..........bddc..
+..........bdbb..
+.........bddc...
+.........bdbb...
+........bddc....
+........bdbb....
+.......bddc.....
+.......bdbb.....
+......bddc......
+......bdbb......
+.....bddc.......
+.....bdbb.......
+....bddc........
+....bdbb........
+...bddc.........
+...bdbb.........
+..bddc..........
+..bdbb..........
+.bddc...........
+.bdbb...........
+b1dc............
+b11c............
+b11c............
+b11c.........cc.
+b11c........cbbc
+b11c........cbbc
+b11c.......bdcc.
+b11c.......bdbb.
+b11c......bddc..
+b11bccc...bdbb..
+b11bbbbcccddc...
+b11bcccbccdbb...
+b11b..ccbddc....
+b11b...ccdbbc...
+b11b...bddcbc...
+b11b...bdbbcbc..
+b11b..bddc.fbc..
+b11b..bdbb.fbf..
+b11b.bddc..fcf..
+b11b.bdbb..fcf..
+b11bbddc...fcf..
+b11bbdbb...fcf..
+b11bddc...cfcfc.
+b11ddbb..cbfcfbc
+b1dddc...cdfffdc
+b1ddbb...cdcfcdc
+cdddc....cbdddbc
+cddbb....cbbbbbc
+cddc.....cbbbbbc
+cdbb.....cbbbbbc
+.cc......cbbbbbc
+.........cbbbbbc
+.........cbbbbbc
+.........cbbbbbc
+.........8bbbbb8
+.........8bbbbb8
+.........6bbbbb6
+..........6bbb6.
+...........666..
+`;
+            case "lK|TDB6r[?N%#SMEd`t}":
+            case "football":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . e e e e e e e e . . . . 
+. . . e e e e e e e e e e . . . 
+. . e e e f e f e f e f e e . . 
+. . . e e e e e e e e e e . . . 
+. . . . e e e e e e e e . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
